70d2d035e0217741bcfe1e4ad198e36e206850b4
Nils Asmussen <nils@script-solution.de>
2012-07-23 19:14:52 +0200

    Now there are ready-to-use cross-compilers for x86_64 hosts.

--- cross/download.sh
+++ cross/download.sh
@@ -12,24 +12,37 @@ if [ "$target" != "x86_32" ] && [ "$target" != "x86_64" ]; then
 	usage $0
 fi
 
-echo "This will download a precompiled cross-compiler into /opt/nre-cross-$target"
-echo "Currently, the only ones available have been built on an Ubuntu 12.04 i686 host."
+echo "This will download a precompiled cross-compiler for target $target into /opt/nre-cross-$target"
+echo "Currently, the only ones available have been built on Ubuntu 12.04 hosts."
 echo "It will probably work on other Ubuntu versions and other distributions" \
 	"as well, but it hasn't been tested yet.\n"
 
-echo -n "Do you think you have a compatible host platform? (yes/no) "
-read dummy
-if [ "$dummy" != "yes" ]; then
-	echo "Ok, please use the build.sh to build your own compiler. Bye!"
-	exit 0
-fi
+host=""
+while [ "$host" = "" ]; do
+	echo "Please select your host platform:"
+	echo "1. Ubuntu 12.04 i686"
+	echo "2. Ubuntu 12.04 x86_64"
+	echo "3. Other (build your own cross-compiler)"
+	echo -n "Your choice (1, 2 or 3): "
+	read choice
+	if [ "$choice" = "1" ]; then
+		host="ubuntu1204-i686"
+	elif [ "$choice" = "2" ]; then
+		host="ubuntu1204-x86_64"
+	elif [ "$choice" = "3" ]; then
+		echo "Ok, please use the build.sh to build your own compiler. Bye!"
+		exit 0
+	else
+		echo "Invalid answer"
+	fi
+done
 
-echo "\nDownloading cross compiler for target $target..."
-wget -c http://os.inf.tu-dresden.de/~nils/nre-cross-ubuntu1204-i686-$target.tar.gz
+echo "\nDownloading cross-compiler for host $host and target $target..."
+wget -c http://os.inf.tu-dresden.de/~nils/nre-cross-$host-$target.tar.gz
 
 echo "\nExtracting it..."
-$SUDO tar xfvz nre-cross-ubuntu1204-i686-$target.tar.gz -C /
+$SUDO tar xfvz nre-cross-$host-$target.tar.gz -C /
 
 echo "\nRemoving archive..."
-rm -f nre-cross-ubuntu1204-i686-$target.tar.gz
+rm -f nre-cross-$host-$target.tar.gz
 
