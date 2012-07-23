3381c834893d3a1451b6035ce856cd0c9e7106d0
Nils-TUD <nils@os.inf.tu-dresden.de>
2012-07-23 14:08:22 +0200

    Unified build.sh and download.sh and changed the readme.

--- cross/download.sh
+++ cross/download.sh
@@ -2,29 +2,27 @@
 
 SUDO=sudo
 
-echo "This will download a precompiled cross-compiler into /opt"
+usage() {
+	echo "Usage: $1 (x86_32|x86_64)" >&2
+	exit
+}
+
+target="$1"
+if [ "$target" != "x86_32" ] && [ "$target" != "x86_64" ]; then
+	usage $0
+fi
+
+echo "This will download a precompiled cross-compiler into /opt/nre-cross-$target"
 echo "Currently, the only ones available have been built on an Ubuntu 12.04 i686 host."
 echo "It will probably work on other Ubuntu versions and other distributions" \
 	"as well, but it hasn't been tested yet.\n"
-/bin/echo -e "\033[1mIf you don't have a compatible platform, please use the build.sh to build" \
-	"the cross-compiler yourself.\033[0m\n"
 
-target=""
-while [ "$target" = "" ]; do
-	echo "Pick target:"
-	echo "1. x86_32"
-	echo "2. x86_64"
-	echo -n "Your choice (1 or 2): "
-	read choice
-
-	if [ "$choice" = "1" ]; then
-		target="x86_32"
-	elif [ "$choice" = "2" ]; then
-		target="x86_64"
-	else
-		echo "Invalid answer"
-	fi
-done
+echo -n "Do you think you have a compatible host platform? (yes/no) "
+read dummy
+if [ "$dummy" != "yes" ]; then
+	echo "Ok, please use the build.sh to build your own compiler. Bye!"
+	exit 0
+fi
 
 echo "\nDownloading cross compiler for target $target..."
 wget -c http://os.inf.tu-dresden.de/~nils/nre-cross-ubuntu1204-i686-$target.tar.gz
