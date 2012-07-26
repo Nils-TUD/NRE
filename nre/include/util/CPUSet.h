1cb9b8d0c9109c5b8f02a8b0769ecbfe67ce73a7
Nils Asmussen <nils@script-solution.de>
2012-07-26 18:33:26 +0200

    Comments

--- nre/include/util/CPUSet.h
+++ nre/include/util/CPUSet.h
@@ -21,21 +21,42 @@
 
 namespace nre {
 
+/**
+ * This class is used to choose CPUs. Currently this is used by the service to let the user
+ * specify on what CPUs it should be available. This class is just a wrapper for BitField that
+ * exists for convenience.
+ */
 class CPUSet {
 public:
+	/**
+	 * The initial settings
+	 */
 	enum Preset {
 		NONE,
 		ALL
 	};
 
+	/**
+	 * Constructor. Preinitializes the set to <preset>
+	 *
+	 * @param preset the CPUs to enable at the beginning (ALL by default)
+	 */
 	explicit CPUSet(Preset preset = ALL) : _cpus() {
 		if(preset == ALL)
 			_cpus.set_all();
 	}
 
+	/**
+	 * @return the CPUs in form of a bitfield
+	 */
 	const BitField<Hip::MAX_CPUS> &get() const {
 		return _cpus;
 	}
+	/**
+	 * Sets the given CPU
+	 *
+	 * @param cpu the CPU
+	 */
 	void set(cpu_t cpu) {
 		_cpus.set(cpu);
 	}
