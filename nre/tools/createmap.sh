27d5e8f473e2561cd132d97729518378a9aadced
Nils Asmussen <nils@script-solution.de>
2012-09-08 18:15:28 +0200

    Added a tool to generate a symbol-map for bochs

---
+++ nre/tools/createmap.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <ELF-file>" >&2
+	exit 1
+fi
+
+nm $1 | grep " T " | awk '
+function filt(arg)
+{
+	c = "c++filt -n "arg
+	c | getline res
+	close(c)
+	return res
+}
+
+{
+	print $1" "filt($3)
+}
+'
