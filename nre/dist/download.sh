81121f4488a58e77fef6d6ed5fc886cf28b701c1
Nils-TUD <nils@os.inf.tu-dresden.de>
2012-09-10 10:27:26 +0200

    Improved the bootscripts and added escape-support

--- nre/dist/download.sh
+++ nre/dist/download.sh
@@ -7,6 +7,11 @@ fi
 
 mkdir -p dist/imgs
 cd dist/imgs
-wget -c http://os.inf.tu-dresden.de/~nils/imgs/bzImage-3.1.0-32
-wget -c http://os.inf.tu-dresden.de/~nils/imgs/initrd-js.lzma
+
+files="bzImage-3.1.0-32 initrd-js.lzma escape.bin escape_cmos.bin escape_fs.bin \
+	escape_romdisk.bin escape.iso"
+
+for f in $files; do
+	wget -c http://os.inf.tu-dresden.de/~nils/imgs/$f
+done
 
