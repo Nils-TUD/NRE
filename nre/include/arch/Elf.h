af8c7c72527400e5a2f30270d3d9401eee525c57
Nils Asmussen <nils@script-solution.de>
2012-10-03 17:49:28 +0200

    Made elf-stuff more flexible

--- nre/include/arch/Elf.h
+++ nre/include/arch/Elf.h
@@ -16,6 +16,8 @@
 
 #pragma once
 
+#include <arch/Types.h>
+
 #ifdef __i386__
 #include <arch/x86_32/Elf.h>
 #elif defined __x86_64__
@@ -24,26 +26,7 @@
 #error "Unsupported architecture"
 #endif
 
-#include <arch/Types.h>
-
 namespace nre {
-
-struct ElfEh {
-	uchar e_ident[16];
-	elf_half_t e_type;
-	elf_half_t e_machine;
-	elf_word_t e_version;
-	elf_addr_t e_entry;
-	elf_off_t e_phoff;
-	elf_off_t e_shoff;
-	elf_word_t e_flags;
-	elf_half_t e_ehsz;
-	elf_half_t e_phentsize;
-	elf_half_t e_phnum;
-	elf_half_t e_shentsize;
-	elf_half_t e_shnum;
-	elf_half_t e_shstrndx;
-};
 
 enum ElfPhFlags {
 	PF_X	= 1 << 0,
@@ -51,38 +34,14 @@ enum ElfPhFlags {
 	PF_R	= 1 << 2,
 };
 
-// TODO thats not nice
-struct ElfPh {
-	elf_word_t p_type;
-#ifdef __x86_64__
-	elf_word_t p_flags;
+#ifdef __i386__
+typedef ElfEh32 ElfEh;
+typedef ElfPh32 ElfPh;
+typedef ElfSh32 ElfSh;
+#elif defined __x86_64__
+typedef ElfEh64 ElfEh;
+typedef ElfPh64 ElfPh;
+typedef ElfSh64 ElfSh;
 #endif
-	elf_off_t p_offset;
-	elf_addr_t p_vaddr;
-	elf_addr_t p_paddr;
-#ifdef __x86_64__
-	elf_addr_t p_filesz;
-	elf_addr_t p_memsz;
-	elf_addr_t p_align;
-#else
-	elf_word_t p_filesz;
-	elf_word_t p_memsz;
-	elf_word_t p_flags;
-	elf_word_t p_align;
-#endif
-};
-
-struct ElfSh {
-	elf_word_t sh_name;
-	elf_word_t sh_type;
-	elf_word_t sh_flags;
-	elf_addr_t sh_addr;
-	elf_off_t sh_offset;
-	elf_word_t sh_size;
-	elf_word_t sh_link;
-	elf_word_t sh_info;
-	elf_word_t sh_addralign;
-	elf_word_t sh_entsize;
-};
 
 }
--- nre/include/arch/x86_32/Elf.h
+++ nre/include/arch/x86_32/Elf.h
@@ -18,7 +18,52 @@
 
 #include <arch/Types.h>
 
-typedef ushort elf_half_t;
-typedef uint elf_word_t;
-typedef uint elf_addr_t;
-typedef uint elf_off_t;
+namespace nre {
+
+typedef ushort elf32_half_t;
+typedef uint elf32_word_t;
+typedef uint elf32_addr_t;
+typedef uint elf32_off_t;
+
+struct ElfEh32 {
+	uchar e_ident[16];
+	elf32_half_t e_type;
+	elf32_half_t e_machine;
+	elf32_word_t e_version;
+	elf32_addr_t e_entry;
+	elf32_off_t e_phoff;
+	elf32_off_t e_shoff;
+	elf32_word_t e_flags;
+	elf32_half_t e_ehsz;
+	elf32_half_t e_phentsize;
+	elf32_half_t e_phnum;
+	elf32_half_t e_shentsize;
+	elf32_half_t e_shnum;
+	elf32_half_t e_shstrndx;
+};
+
+struct ElfPh32 {
+	elf32_word_t p_type;
+	elf32_off_t p_offset;
+	elf32_addr_t p_vaddr;
+	elf32_addr_t p_paddr;
+	elf32_word_t p_filesz;
+	elf32_word_t p_memsz;
+	elf32_word_t p_flags;
+	elf32_word_t p_align;
+};
+
+struct ElfSh32 {
+	elf32_word_t sh_name;
+	elf32_word_t sh_type;
+	elf32_word_t sh_flags;
+	elf32_addr_t sh_addr;
+	elf32_off_t sh_offset;
+	elf32_word_t sh_size;
+	elf32_word_t sh_link;
+	elf32_word_t sh_info;
+	elf32_word_t sh_addralign;
+	elf32_word_t sh_entsize;
+};
+
+}
