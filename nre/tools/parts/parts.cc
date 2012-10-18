d87a20452fd8120bcfe316ca8e8c2efd4401e347
Nils-TUD <nils@os.inf.tu-dresden.de>
2012-10-18 10:25:19 +0200

    Include unistd.h in parts (is required on some systems)

--- nre/tools/parts/parts.cc
+++ nre/tools/parts/parts.cc
@@ -1,9 +1,26 @@
+/*
+ * Copyright (C) 2012, Nils Asmussen <nils@os.inf.tu-dresden.de>
+ * Economic rights: Technische Universitaet Dresden (Germany)
+ *
+ * This file is part of NRE (NOVA runtime environment).
+ *
+ * NRE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * NRE is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License version 2 for more details.
+ */
+
 #include <iostream>
 #include <stdint.h>
 #include <cstdio>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
 
 /* offset of partition-table in MBR */
 #define PART_TABLE_OFFSET	0x1BE
@@ -61,4 +78,3 @@ int main(int argc,char **argv) {
 	close(disk);
 	return 0;
 }
-
